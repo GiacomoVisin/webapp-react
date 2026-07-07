148d8fd42cec00825b9fcf724bb3f79881ac009a
GiacomoVisin <killerboss1150@gmail.com>
2026-07-07 23:39:56 +0200

    Add axios for axios api and implement the card in the home page using the map

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,22 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+
+
 export default function Home() {
+
+    const [movies, setMovies] = useState([]);
+    const api = "http://localhost:3000/index";
+    useEffect(() => {
+        axios.get(api)
+            .then(response => {
+                setMovies(response.data);
+            })
+            .catch(error => {
+                console.error("Errore durante il recupero dei film:", error);
+            })
+
+    }, []);
 
     return (
         <>
@@ -7,6 +25,12 @@ export default function Home() {
                 <p>
                     scopri i nostri Film e le loro caratteristiche, cliccando sul pulsante "PIU' DETTAGLI" potrai accedere ad una pagina dedicata al film selezionato.
                 </p>
+            </div>
+
+            <div className="container d-flex flex-wrap justify-content-center">
+                {movies.map((movie)=>(
+                    <MovieCard movie={movie} key={movie.id} />
+                ))}
             </div>
         </>
     );
