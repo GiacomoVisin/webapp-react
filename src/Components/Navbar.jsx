87865563fec8febcd99375c71cd03de66daba1e0
GiacomoVisin <killerboss1150@gmail.com>
2026-07-15 23:19:42 +0200

    add style to the navbar and finishing the app.css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -17,9 +17,9 @@ export default function NavBar({ Loader }) {
 
     return (
 
-        <nav style={{ display: "flex", gap: "50px", padding: "10px", background: "#eee", textDecoration: "none" }}>
-            <NavLink to="/" className="navlink pt-1" style={{ color: "black" }}> HOME </NavLink>
-            <NavLink to="/AddMoviesForm" className="navlink pt-1" style={{ color: "black" }}> AGGIUNGI FILM</NavLink>
+        <nav style={{ display: "flex", gap: "50px", padding: "10px",  background: "linear-gradient(to right, #371b4c, #1d2e66)", textDecoration: "none" }}>
+            <NavLink to="/" className="navlink pt-1 border"> HOME </NavLink>
+            <NavLink to="/AddMoviesForm" className="navlink pt-1 border"> AGGIUNGI FILM</NavLink>
             <button className={`btn ${showLoader ? "btn-primary" : "btn-secondary"} right`} onClick={switchLoader}> {showLoader ? "Disable Loader" : "Enable Loader"} </button>
         </nav>
     );
